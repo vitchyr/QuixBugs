1fd2c3402e1a2aa3ff9ec22d7ba82c07b59996c2
Vitchyr Pong <vitchyr@gmail.com>
2021-11-17 17:43:30 -0800

    a few more bitcount tests

--- python_programs/bitcount_test.py
+++ python_programs/bitcount_test.py
@@ -4,6 +4,9 @@ from .bitcount import bitcount
 def test_main():
     assert bitcount(127) == 7
     assert bitcount(128) == 1
+    assert bitcount(1) == 1
+    assert bitcount(0) == 0
+    assert bitcount(30) == 4
 
 
 if __name__ == "__main__":
